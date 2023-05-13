cd4dcb7c5e5be8bda7647fab81ad609d0bf99b4d
olayinka06-dev <olayinkaconsult06@gmail.com>
2023-05-13 10:38:44 -0700

    creating the Service Page

---
+++ src/components/Services page/ServiceData.js
@@ -0,0 +1,76 @@
+const ServiceData = [
+    {
+        src: "",
+        name: "",
+        languages: [
+            "HTML",
+            "CSS",
+            "React",
+            "Node Js",
+            "Mongo DB",
+            "Mongoose",
+        ]
+    },
+    {
+        src: "",
+        name: "",
+        languages: [
+            "HTML",
+            "CSS",
+            "React",
+            "Node Js",
+            "Mongo DB",
+            "Mongoose",
+        ]
+    },
+    {
+        src: "",
+        name: "",
+        languages: [
+            "HTML",
+            "CSS",
+            "React",
+            "Node Js",
+            "Mongo DB",
+            "Mongoose",
+        ]
+    },
+    {
+        src: "",
+        name: "",
+        languages: [
+            "HTML",
+            "CSS",
+            "React",
+            "Node Js",
+            "Mongo DB",
+            "Mongoose",
+        ]
+    },
+    {
+        src: "",
+        name: "",
+        languages: [
+            "HTML",
+            "CSS",
+            "React",
+            "Node Js",
+            "Mongo DB",
+            "Mongoose",
+        ]
+    },
+    {
+        src: "",
+        name: "",
+        languages: [
+            "HTML",
+            "CSS",
+            "React",
+            "Node Js",
+            "Mongo DB",
+            "Mongoose",
+        ]
+    },
+]
+
+export default ServiceData;
---
+++ src/components/Services page/Services.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+import ServiceData from './ServiceData';
+
+const Services = () => {
+  return (
+    <Wrapper>
+
+    </Wrapper>
+  )
+}
+const Wrapper = styled.div`
+    
+
+`
+export default Services
