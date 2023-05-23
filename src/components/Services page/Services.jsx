d5dbbb526397a79c8a44dd4b773ae67cab840f1e
olayinka06-dev <olayinkaconsult06@gmail.com>
2023-05-23 06:07:34 -0700

    creating the service page

--- src/components/Services page/Services.jsx
+++ src/components/Services page/Services.jsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import website from '../images/Top-6-Front-End-Web-Development-Tools-to-Increase-Your-Productivity-in-2020-1.jpg';
+import app from '../images/mobileapp.jpg';
+import '../../index.css';
 import ServiceData from './ServiceData';
+import {IoIosArrowForward, IoMdBookmark} from 'react-icons/io';
 
 const Services = () => {
 
@@ -12,52 +16,115 @@ const Services = () => {
   return (
     <Wrapper>
       <section id="services" className="services">
-      <div className="container" data-aos="fade-up">
+        <div className="container" data-aos="fade-up">
 
-        <div className="section-title">
-          <h2>Services</h2>
-          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
-        </div>
+          <div className="section-title">
+            <h2>Services</h2>
+            <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+          </div>
 
-        <div className="row">
+          <div className="row">
 
-        {
-          services && (
-            services.map((service, index)=> (
-              <div key={index} className="" data-aos="zoom-in">
-                <div class="">
-                  <div class="icon">
-                    <svg width="100" height="100" viewBox="0 0 600 600" xmlns="http://www.w3.org/2000/svg">
-                      <path stroke="none" stroke-width="0" fill="#f5f5f5" d={service.src}></path>
-                    </svg>
-                    <i className=""></i>
-                  </div>
-                  <h4><a href="">{service.name}</a></h4>
-                  {
-                    service.languages.map((language, index)=>(
-                      <ul>
-                        <li key={index}>{language}</li>
-                      </ul>
-                    ))
-                  }
-
+            <div  className="card" data-aos="zoom-in">
+              <div class="cards">
+                <div class="icon">
+                  <img src={website} alt={website} />
                 </div>
+                <div className="info">
+                  <h4><a href="/about">Website Development</a></h4>
+                  {
+                    services && (
+                      services[0].website.map((service, index) => (
+                        <ul key={index}>
+                          <li><IoIosArrowForward/>{service}</li>
+                        </ul>
+                      ))
+                    )
+                  }
+                  {
+                    services && (
+                      services[0].languages.map((language, index) => (
+                        <ul key={index}>
+                          <li><IoMdBookmark/>{language}</li>
+                        </ul>
+                      ))
+                    )
+                  }
+                </div>
+              </div>
             </div>
-  
-            ))
-          )
-        }
-
+            <div  className="card" data-aos="zoom-in">
+              <div class="cards">
+                <div class="icon">
+                  <img src={app} alt={app} />
+                </div>
+                <div className="info">
+                  <h4><a href="/about">App Development</a></h4>
+                  {
+                    services && (
+                      services[1].app.map((service, index) => (
+                        <ul key={index}>
+                          <li><IoIosArrowForward/>{service}</li>
+                        </ul>
+                      ))
+                    )
+                  }
+                  {
+                    services && (
+                      services[1].languages.map((language, index) => (
+                        <ul key={index}>
+                          <li><IoMdBookmark/>{language}</li>
+                        </ul>
+                      ))
+                    )
+                  }
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-
-      </div>
-    </section>
+      </section>
     </Wrapper>
   )
 }
-const Wrapper = styled.div`
-    
-    .services .icon-box {
+const Wrapper = styled.section`
+.services{
+  width: 100%;
+  padding-top: 40px;
+  padding-right: 10px;
+  padding-bottom: 20px;
+  background-color: var(--sectionbackground);
+}
+.row {
+  width: 100%;
+  display: flex;
+  gap: 80px;
+  padding: 20px 60px;
+  
+}
+.card {
+  width: 100%;
+  box-shadow: 0px 5px 10px #d4d4d4, 0px 2px 2px #d4d4d4, 0px 2px 2px #d4d4d4, 0px 2px 2px #d4d4d4;
+}
+.info{
+  padding: 20px;
+}
+.icon img{
+  width: 100%;
+  height: 35vh;
+  transition: all 0.5s ease;
+  opacity: 100%;
+}
+
+.icon:hover img {
+  transform: scale(3.05);
+  opacity: 90%;
+}
+ul {
+  text-decoration: none;
+  list-style: none;
+}
+.services .icon-box {
   text-align: center;
   padding: 70px 20px 80px 20px;
   transition: all ease-in-out 0.3s;
@@ -75,24 +142,6 @@ const Wrapper = styled.div`
   transition: ease-in-out 0.3s;
   position: relative;
 }
-
-.services .icon-box .icon i {
-  font-size: 36px;
-  transition: 0.5s;
-  position: relative;
-}
-
-.services .icon-box .icon svg {
-  position: absolute;
-  top: 0;
-  left: 0;
-}
-
-.services .icon-box .icon svg path {
-  transition: 0.5s;
-  fill: #f5f5f5;
-}
-
 .services .icon-box h4 {
   font-weight: 600;
   margin: 10px 0 15px 0;
@@ -103,89 +152,15 @@ const Wrapper = styled.div`
   color: #45505b;
   transition: ease-in-out 0.3s;
 }
-
-.services .icon-box p {
-  line-height: 24px;
-  font-size: 14px;
-  margin-bottom: 0;
+.cards {
+    position: relative;
+    overflow: hidden;
+}
+.cards:focus-within {
+    outline: #68becd dashed 3px;
+    outline-offset: 3px;
 }
 
-.services .icon-box:hover {
-  border-color: #fff;
-  box-shadow: 0px 0 35px 0 rgba(0, 0, 0, 0.08);
-}
-
-.services .iconbox-blue i {
-  color: #47aeff;
-}
-
-.services .iconbox-blue:hover .icon i {
-  color: #fff;
-}
-
-.services .iconbox-blue:hover .icon path {
-  fill: #47aeff;
-}
-
-.services .iconbox-orange i {
-  color: #ffa76e;
-}
-
-.services .iconbox-orange:hover .icon i {
-  color: #fff;
-}
-
-.services .iconbox-orange:hover .icon path {
-  fill: #ffa76e;
-}
-
-.services .iconbox-pink i {
-  color: #e80368;
-}
-
-.services .iconbox-pink:hover .icon i {
-  color: #fff;
-}
-
-.services .iconbox-pink:hover .icon path {
-  fill: #e80368;
-}
-
-.services .iconbox-yellow i {
-  color: #ffbb2c;
-}
-
-.services .iconbox-yellow:hover .icon i {
-  color: #fff;
-}
-
-.services .iconbox-yellow:hover .icon path {
-  fill: #ffbb2c;
-}
-
-.services .iconbox-red i {
-  color: #ff5828;
-}
-
-.services .iconbox-red:hover .icon i {
-  color: #fff;
-}
-
-.services .iconbox-red:hover .icon path {
-  fill: #ff5828;
-}
-
-.services .iconbox-teal i {
-  color: #11dbcf;
-}
-
-.services .iconbox-teal:hover .icon i {
-  color: #fff;
-}
-
-.services .iconbox-teal:hover .icon path {
-  fill: #11dbcf;
-}
 
 
 `
