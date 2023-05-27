d62b6688e3cdbb06b569ab09e1cc1afe70e2f0f4
olayinka06-dev <olayinkaconsult06@gmail.com>
2023-05-27 16:03:06 -0700

    creating the testimonial and resume page

--- src/components/Testimonial page/Testimonial.jsx
+++ src/components/Testimonial page/Testimonial.jsx
@@ -7,18 +7,18 @@ import 'swiper/swiper.min.css';
 const Testimonial = () => {
     useEffect(() => {
         new Swiper('.testimonials-slider', {
-          speed: 600,
-          loop: true,
-          autoplay: {
-            delay: 5000,
-            disableOnInteraction: false
-          },
-          slidesPerView: 3,
-          pagination: {
-            el: '.swiper-pagination',
-            type: 'bullets',
-            clickable: true
-          }
+            speed: 600,
+            loop: true,
+            slidesPerView: 3,
+            pagination: {
+              el: '.swiper-pagination',
+              type: 'bullets',
+              clickable: true,
+            },
+            autoplay: {
+              delay: 5000,
+              disableOnInteraction: false,
+            },
         });
       }, []);
   return (
