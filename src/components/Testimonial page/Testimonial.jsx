2faa0b294cbdc05e88728c23e2a691bbd61b1dba
olayinka06-dev <olayinkaconsult06@gmail.com>
2023-06-03 05:42:44 -0700

    making some Adjustment

--- src/components/Testimonial page/Testimonial.jsx
+++ src/components/Testimonial page/Testimonial.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import Swiper from 'swiper';
+// import { Pagination } from 'swiper';
 import 'swiper/swiper.min.css';
 
 const Testimonial = () => {
